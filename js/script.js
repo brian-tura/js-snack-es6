5a494e4b381efccdbfea15155b87dcf03a769593
brian-tura <b.tura2002@gmail.com>
2025-03-31 10:03:33 +0200

    Snack 2 Done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,57 @@
-const bikes = [
-    {
-        name: "Mario",
-        weight: 20,
-    },
-    {
-        name: "Ale",
-        weight: 22,
-    },
-    {
-        name: "Giulia",
-        weight: 19,
-    }
+// const bikes = [
+//     {
+//         name: "Mario",
+//         weight: 20,
+//     },
+//     {
+//         name: "Ale",
+//         weight: 22,
+//     },
+//     {
+//         name: "Giulia",
+//         weight: 19,
+//     }
+// ];
+
+// let check = bikes[2].weight;
+// for (let i = 0; i < bikes.length; i++) {
+//     if (bikes[i].weight < check) {
+//         check = bikes[i].weight;
+//     }
+// }
+
+// console.log(check);
+
+const football = [
+        {
+            name: "Mario",
+            points: 0,
+            fouls: 0,
+        },
+        {
+            name: "Ale",
+            points: 0,
+            fouls: 0,
+        },
+        {
+            name: "Giulia",
+            points: 0,
+            fouls: 0,
+        }
 ];
 
-let check = bikes[2].weight;
-for (let i = 0; i < bikes.length; i++) {
-    if (bikes[i].weight < check) {
-        check = bikes[i].weight;
-    }
-}
+for(let i=0;i<football.length;i++){
+    football[i].points = Math.floor(Math.random()*10);
+    football[i].fouls = Math.floor(Math.random()*10);
+};
 
-console.log(check);
+const newFootball = [];
+for(let j=0;j<football.length;j++){
+    newFootball.push({
+        name: football[j].name,
+        fouls: football[j].fouls,
+    })
+};
+
+console.log(newFootball);
+
